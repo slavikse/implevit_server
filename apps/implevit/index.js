19a3a8f9067f0ddc4dc3cb0da3f2137b3061b9e6
slavikse <gslavikse@gmail.com>
2018-12-13 18:06:54 +0300

    configure deploy

--- apps/implevit/index.js
+++ apps/implevit/index.js
@@ -2,13 +2,8 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const path = require('path');
 
+const dist = path.resolve(`${__dirname}/app/dist`);
 const port = 80;
-
-const dist = path.resolve('/apps/implevit/app/dist');
-
-console.log('pwd', process.cwd());
-console.log('__dirname ', __dirname);
-console.log('dist', dist);
 
 const app = new Koa();
 app.use(serve(dist));
