1e3b8e2b1913d2b68260044787269e6f99d67fa7
slavikse <gslavikse@gmail.com>
2018-12-13 16:44:47 +0300

    configure deploy

--- apps/implevit/index.js
+++ apps/implevit/index.js
@@ -4,7 +4,7 @@ const serve = require('koa-static');
 const staticPort = 80;
 
 const app = new Koa();
-app.use(serve('app/dist'));
+app.use(serve('./app/dist'));
 
 app.listen(staticPort, () => {
   console.log(`Server Listen Port: ${staticPort}`);
