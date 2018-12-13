2c99cd623a492ddd0e377e410c26c310306091f3
slavikse <gslavikse@gmail.com>
2018-12-13 18:02:38 +0300

    configure deploy

--- apps/implevit/index.js
+++ apps/implevit/index.js
@@ -6,6 +6,8 @@ const port = 80;
 
 const dist = path.resolve('/apps/implevit/app/dist');
 
+console.log('pwd', process.cwd());
+console.log('__dirname ', __dirname);
 console.log('dist', dist);
 
 const app = new Koa();
